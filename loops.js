46b7af77e007fcef19eb29d6e295291164c2ec67
Joon Jang <joon.jang95@gmail.com>
2018-08-13 22:25:42 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,24 @@
 function forLoop(array) {
-  for (var i = 0; i < 26; i++) {
+  for (var i = 0; i < 25; i++) {
     if (i === 1) {
       array.push("I am 1 strange loop.");
     } else {
       array.push(`I am ${i} strange loops.`);
     }
   }
+  
+  return array
 }
 
-return forLoop
+function whileLoop(number) {
+  while (number > 0) {
+    console.log(--number);
+  }
+  return 'done'
+}
+
+function doWhileLoop(array) {
+  do {
+    array.length 
+  } while (array.length > 0 && incrementVariable()); 
+}
